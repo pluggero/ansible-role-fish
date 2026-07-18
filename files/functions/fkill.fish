b9586d512cfc746db20cdec238cc980a10b46cbd
PluggeRo <robin@plugge-mail.de>
2026-07-19 00:43:33 +0200

    fix: Removed default fish functions

--- files/functions/fkill.fish
+++
@@ -1,8 +0,0 @@
-# Managed by Ansible, please don't edit manually
-function fkill
-    # List processes, pipe to fzf, extract PID, and kill the selected process
-    set pid (ps aux | fzf | awk '{print $2}')
-    if test -n "$pid"
-        kill -9 $pid
-    end
-end
