a364259d4971fb78ceb6c83bac250356c25bd006
Robin Plugge <55260737+pluggero@users.noreply.github.com>
2026-07-19 00:52:57 +0200

    fix: Removed default fish functions (#103)

--- files/functions/clear_and_repaint.fish
+++
@@ -1,8 +0,0 @@
-# Managed by Ansible, please don't edit manually
-# This function is a workaround for the clear-screen issue on remote hosts.
-function clear_and_repaint
-    # Clear the terminal
-    clear
-    # Redraw the prompt
-    commandline -f repaint
-end
--- files/functions/ttake.fish
+++
@@ -1,4 +0,0 @@
-# Managed by Ansible, please don't edit manually
-function ttake --description="Creates a temporary directory and switches into it."
-    cd $(mktemp -d)
-end
